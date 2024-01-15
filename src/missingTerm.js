9c2f15d65f5916bd180e62375959adff58af8fdf
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2024-01-15 12:08:04 +0100

    refactor: refactor prod code

--- src/missingTerm.js
+++ src/missingTerm.js
@@ -1,20 +1,26 @@
 function missingTerm(list) {
-  // for (let i = 0; i < list.length; i++) {
-  //   console.log('list: ', list[i]);
-  // }
   const lowest = list[0];
   const highest = list[2];
-  const median = list[2] / list[0];
+  const division = list[2] / list[0];
   console.log('lowest: ', lowest);
   console.log('highest: ', highest);
-  console.log('median: ', median);
+  console.log('division: ', division);
 
-  if (list.toString() === '1,5,7') {
-    return 3;
-  } else if (list.toString() === '1,4,10') {
-    return 7;
+  for (let i = 0; i < list.length; i++) {
+    console.log('list: ', list[i]);
+    console.log('list[i+1]: ', list[i + 1]);
+    console.log(list[i] + division != list[i + 1]);
+    if (list[i] + division != list[i + 1]) {
+      return list[i + 1];
+    }
   }
-  return 2;
+
+  // if (list.toString() === '1,5,7') {
+  //   return 3;
+  // } else if (list.toString() === '1,4,10') {
+  //   return 7;
+  // }
+  // return 2;
 }
 
 module.exports = missingTerm;
