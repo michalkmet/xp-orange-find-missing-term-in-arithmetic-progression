151d96bf00a01fd7f929171b5db4a8ac571ae67a
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2024-01-15 12:25:04 +0100

    refactor: refactor

--- src/missingTerm.js
+++ src/missingTerm.js
@@ -1,13 +1,7 @@
 function missingTerm(list) {
   const highest = list[list.length - 1];
   const division = Math.floor(highest / list.length);
-  console.log('highest: ', highest);
-  console.log('division: ', division);
-
   for (let i = 0; i < list.length; i++) {
-    console.log('list: ', list[i]);
-    console.log('list[i+1]: ', list[i + 1]);
-    console.log(list[i] + division != list[i + 1]);
     if (list[i] + division != list[i + 1]) {
       return list[i] + division;
     }
